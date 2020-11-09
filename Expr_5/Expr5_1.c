63c3cb8228abf8ee11e4aeb44d66f1473c31bd12
SuperMaxine <2586540653@qq.com>
2020-11-09 22:34:57 +0800

    Add comment to Expr_5_1

--- Expr_5/Expr5_1.c
+++ Expr_5/Expr5_1.c
@@ -7,18 +7,19 @@
 #include <stdlib.h>
 #include<sys/wait.h>
 
+// 开启消息队列函数
 int open_queue(int keyval) 
 {
 int qid;
 qid = msgget(keyval, IPC_CREAT | 0666);
     if (qid == -1) {
         perror("Failed in calling msgget");
-        return (-1);
+        exit(EXIT_FAILURE);
     }
     return (qid);
 }
 
-
+// 定义消息队列数据结构
 struct msg_st
 {
     long int msg_type;
@@ -26,44 +27,42 @@ struct msg_st
 };
 
 int main(){
-    int msgid = -1;
-    msgid = open_queue(1234);
-    if (msgid == -1)
-    {
-        fprintf(stderr, "msgget failed width error\n");
-        exit(EXIT_FAILURE);
-    }
+    // 开启消息队列
+    int msgid = open_queue(1234);
+
     //子进程1
     if(fork() == 0){
         struct msg_st b;
-        long int msgtype = 0;
-        b.msg_type = 1;
+        b.msg_type = 1; // 表示获取队列中第一个可用的消息
+
+        // 循环接受消息队列中的消息
         while(1){
-            if(msgrcv(msgid, (void *)&b, 100, msgtype, 0) == -1){
+            if(msgrcv(msgid, (void *)&b, 100, 0, 0) == -1){
                 // printf("err\n");
             }else{
-                if(!strcmp(b.text, "exit"))break;
-                else printf("The message is %s\n", b.text);
+                // 成功接受到消息
+                if(!strcmp(b.text, "exit"))break; // 如果是“exit”则退出循环
+                else printf("The message is %s\n", b.text); // 否则打印消息
             }
         }
-        exit(0);
+        exit(0); // 退出子进程
     }else{
         //子进程2
-        if(fork() > 0)wait(NULL);
+        if(fork() > 0)wait(NULL); // 为了使子进程中的scanf可以获得终端中的输入，主进程要阻塞等待子进程的结束
         else{
-            int msgid = msgget((key_t)1234, IPC_CREAT|0666);
             struct msg_st a;
-            a.msg_type = 1;
-            scanf("%s",a.text);
+            a.msg_type = 1; // 表示获取队列中第一个可用的消息
+            scanf("%s",a.text); // 从终端获取输入
+            if(!strcmp(a.text, "exit"))exit(0); // 如果输入内容为“exit”则直接退出子进程
             while(1){
-                msgsnd(msgid, (void *)&a, 100, 0);
-                scanf("%s",a.text);
+                msgsnd(msgid, (void *)&a, 100, 0); // 发送终端输入的内容到消息队列
+                scanf("%s",a.text); // 从终端输入下一条消息
                 // printf("input %s", a.text);
-                if(!strcmp(a.text, "exit"))break;
+                if(!strcmp(a.text, "exit"))break; // 如果输入内容为“exit”则直接退出子进程
             }
-            exit(0);
+            exit(0); // 退出子进程
         }
     }
-    // wait(NULL);
+
     return 0;
 }
